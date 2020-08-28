90acf5e5526a572428bf9f02e41828364f5b220d
wxyy <ks_196@126.com>
2020-08-28 18:35:10 +0800

    use python load file

--- flaskApi/app.py
+++ flaskApi/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ def index():
 
 @app.route('/getData')
 def getData():
-    return {'data': [{"key": "1b81910b-f90d-4cd5-8489-5175581d5fc2", "webName": "网易邮箱", "name": "ks_196@126.com", "pwd": "5b88e75704ef0cbf963e7d440aae0a3b"}]}
+    with open(r'D:\website\wxyy-static\data-source.json', 'rb') as f:
+        data = json.load(f)
+    return {'data': data}
     # return [{"key": "1b81910b-f90d-4cd5-8489-5175581d5fc2", "webName": "网易邮箱", "name": "ks_196@126.com", "pwd": "5b88e75704ef0cbf963e7d440aae0a3b"}]
 
 

8940198f710ff8a83b3fe309dc110f17548e2aa5
wxyy <ks_196@126.com>
2020-08-31 16:33:16 +0800

    add query passwordlist with params

--- flaskApi/app.py
+++ flaskApi/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -9,12 +9,13 @@ def index():
     return 'hello flask'
 
 
-@app.route('/getData')
+@app.route('/getData', methods=['GET'])
 def getData():
+    webName = request.args.get("webName")
     with open(r'D:\website\wxyy-static\data-source.json', 'rb') as f:
         data = json.load(f)
-    return {'data': data}
-    # return [{"key": "1b81910b-f90d-4cd5-8489-5175581d5fc2", "webName": "网易邮箱", "name": "ks_196@126.com", "pwd": "5b88e75704ef0cbf963e7d440aae0a3b"}]
+        filterData = [o for o in data if o['webName'].find(webName) != -1]
+    return {'data': filterData}
 
 
 if __name__ == '__main__':
